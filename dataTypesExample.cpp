a167f216078db3b1d4b1221c14bc9c54a4dd2bf1
Rushda Abbasi <rush.cs19@gmail.com>
2026-01-13 16:48:40 +0500

    Add files via upload

---
+++ dataTypesExample.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 10;
+    float pi = 3.14;
+    char n = 'r';
+    cout<<"The value of a is: "<<a;
+    cout<<"\nThe value of pi is : "<<pi;
+    cout<<"\ncharacter n is : "<<n<<"\n";
+    return 0;
+}
