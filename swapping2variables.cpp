68a19d2919d05c60ca9067a6d15aabee20ad3687
Rushda Abbasi <rush.cs19@gmail.com>
2026-04-10 15:48:18 +0500

    Add files via upload

---
+++ swapping2variables.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 2;
+    int b = 1;
+
+    cout<<"Before swapping: a = " << a <<" and b = " << b <<endl;
+
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout<<"After swapping: a = "<< a << " and b = "<< b << endl;
+    return 0;
+}
